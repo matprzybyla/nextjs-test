80f5813649f1ccc64b8c12ca7929298d721539b0
Mateusz Przybyła <mateusz.przybyla@outlook.com>
2024-05-21 09:32:46 +0200

    Test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <div>
         <Header></Header>
         <div>home</div>
-        <iframe src="https://rsmateusz.ngrok.io/bookingswidget" height="1000" width="360" title="booking widget"></iframe>
+        <iframe src="https://rsmateusz.ngrok.io/bookingswidget" height="1000" width="360" title="booking widget" style={{overflow:"hidden"}}></iframe>
         <Footer></Footer>
       </div>
     </>
