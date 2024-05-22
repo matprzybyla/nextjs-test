b8212de64e6ef576c76c52375a11440392e45caa
Mateusz Przybyła <mateusz.przybyla@outlook.com>
2024-05-22 08:50:17 +0200

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ export default function Home() {
           <iframe src="https://rsmateusz.ngrok.io/bookingswidget" title="booking widget" scrolling='yes' height={500}></iframe>
         </div> */}
 
-          <iframe src="https://rsmateusz.ngrok.io/bookingswidget" title="booking widget" style={{width: "320px", height: "90dvh"}}></iframe>
+          <iframe src="https://rsmateusz.ngrok.io/bookingswidget" title="booking widget" style={{width: "320px", height: "80dvh"}}></iframe>
         <Footer></Footer>
       </div>
     </>
