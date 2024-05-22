d50f4f09add0ea9fa75d96f3e7e17400197b8eb4
Mateusz Przybyła <mateusz.przybyla@outlook.com>
2024-05-22 08:23:28 +0200

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,11 +17,11 @@ export default function Home() {
       <div>
         <Header></Header>
         <div>home</div>
-        {/* <div style={{WebkitOverflowScrolling: "touch", overflow: "scroll", height: "1000px", width:"320px", padding: 5, background: "red",position: "fixed", right: 0,  bottom: 0,  left: 0,  top: 0}}>
+        <div style={{WebkitOverflowScrolling: "touch", overflow: "scroll", height: "500px", width:"320px", padding: 5, background: "red"}}>
           <iframe src="https://rsmateusz.ngrok.io/bookingswidget" title="booking widget" style={{width: "100%", height: "100%", overflow: "visible"}}></iframe>
-        </div> */}
+        </div>
 
-          <iframe src="https://rsmateusz.ngrok.io/bookingswidget" title="booking widget" style={{width: "320px", height: "500px", overflow: "scroll"}}></iframe>
+          {/* <iframe src="https://rsmateusz.ngrok.io/bookingswidget" title="booking widget" style={{width: "320px", height: "500px", overflow: "scroll"}}></iframe> */}
         <Footer></Footer>
       </div>
     </>
