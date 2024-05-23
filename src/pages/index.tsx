f85d023ed11d5ecdfb3c6fe965a871bdb15e2e03
Mateusz Przybyła <mateusz.przybyla@outlook.com>
2024-05-23 07:58:36 +0200

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ export default function Home() {
           <iframe src="https://rsmateusz.ngrok.io/bookingswidget" title="booking widget" scrolling='yes' height={500}></iframe>
         </div> */}
 
-          <iframe src="https://thecrab.munu.shop/bookingswidget" title="booking widget" style={{width: "300px", height: "80dvh", WebkitOverflowScrolling: "touch", overflow: "auto"}}></iframe>
+          {/* <iframe src="https://thecrab.munu.shop/bookingswidget" title="booking widget" style={{width: "300px", height: "80dvh", WebkitOverflowScrolling: "touch", overflow: "auto"}}></iframe> */}
         <Footer></Footer>
       </div>
     </>
